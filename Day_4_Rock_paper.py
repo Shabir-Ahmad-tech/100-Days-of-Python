077be32d072fa4049d0df763c75cdc39d93de4fb
Shabir Ahmad <shabirahmad.tech@gmail.com>
2026-01-16 13:56:18 +0500

    Some changes in day 4

--- Day_4_Rock_paper.py
+++ Day_4_Rock_paper.py
@@ -58,6 +58,57 @@ else:
     else:
         print("You lose!")
 
+# rock = """
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# """
+
+# paper = """
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# """
+
+# scissors = """
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# """
+# game_images = [rock, paper, scissors]
+
+# user_choice = int(input("Enter your choise: 0 for Rock, 1 for Paper 2 for Scissors: "))
+# if user_choice >= 0 and user_choice <= 2:
+#     print(game_images[user_choice])
+
+# computer_choice = rd.randint(0, 2)
+# print("---Computer choose: ---")
+# print(game_images[computer_choice])
+
+# if user_choice >= 3 and user_choice < 0:
+#     print("invalid number.... You lose! ....")
+# elif user_choice == 0 and computer_choice == 2:
+#     print("You win!")
+# elif user_choice == 2 and computer_choice == 0:
+#     print("You lose!")
+# elif user_choice > computer_choice:
+#     print("You win!")
+# elif user_choice < computer_choice:
+#     print("You lose!")
+# elif user_choice == computer_choice:
+#     print("It's a draw!")
+
+
+
 #==========================
 #----Code Challenge 01-----
 #==========================
